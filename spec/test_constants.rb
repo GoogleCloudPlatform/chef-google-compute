42882d8ba1b34a8cc3ac843c5262d0f89f07c4f4
Nathan McKinley <nmckinley@google.com>
2018-10-11 22:52:15 +0000

    WIP: Add interconnect attachments.

--- spec/test_constants.rb
+++ spec/test_constants.rb
@@ -387,6 +387,33 @@ module GoogleTests
       'test name#4 data'
     ].freeze
 
+    # Constants for: InterconnectAttachment.project
+    IA_PROJECT_DATA = [
+      'test project#0 data',
+      'test project#1 data',
+      'test project#2 data',
+      'test project#3 data',
+      'test project#4 data'
+    ].freeze
+
+    # Constants for: InterconnectAttachment.region
+    IA_REGION_DATA = %w[
+      selflink(resource(region0))
+      selflink(resource(region1))
+      selflink(resource(region2))
+      selflink(resource(region3))
+      selflink(resource(region4))
+    ].freeze
+
+    # Constants for: InterconnectAttachment.name
+    IA_NAME_DATA = [
+      'test name#0 data',
+      'test name#1 data',
+      'test name#2 data',
+      'test name#3 data',
+      'test name#4 data'
+    ].freeze
+
     # Constants for: MachineType.project
     MT_PROJECT_DATA = [
       'test project#0 data',
